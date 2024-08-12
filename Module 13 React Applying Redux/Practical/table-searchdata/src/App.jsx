b7e2cec4ce02c7b253e90bbb3c0bb64e5ffb5d4a
RageshriTevani <147610242+RageshriTevani@users.noreply.github.com>
2024-08-13 00:18:56 +0530

    Add files via upload

---
+++ Module 13 React Applying Redux/Practical/table-searchdata/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Tablesearch from './Tablesearch'
+
+export default function App() {
+  return (
+    <div className='App'>
+      <header className='header'>
+        {/* <h1>Table Search Example</h1> */}
+        <Tablesearch/>
+      </header>
+
+    </div>
+  )
+}
